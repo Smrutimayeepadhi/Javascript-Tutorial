6d59fd3ad043fad452de89bdbf5a43f0e74461ee
smrutimayee <smrutimayeept44@gmail.com>
2022-07-27 15:20:25 +0100

    JS-1 file added

---
+++ JS-1.js
@@ -0,0 +1,111 @@
+// console.log("Hello IT world, I am going to be in..")
+// var myName="Smruti"; camel Case
+// console.log(myname);
+// var x = true
+// var myAge=29
+// console.log(myAge);
+// console.log(typeof(myAge));
+// console.log(typeof(x));
+// x= "10"
+// var y = 10
+// x = 10
+
+// if(x===y){
+//     console.log("both are equal")
+// }
+// else{
+//     console.log("not equal")
+// }
+// console.log(3+8)
+// console.log("remainder operator " + 81%8)
+// console.log("remainder operator" + 81-8)//should not be written like this
+
+// var num = 15;
+// var newnum = num++;
+// console.log(num);
+// console.log(newnum);
+// var num = 15;
+// var newnum = num++;
+// var num2 = ++ num; //var num2 = --num +5
+// console.log(num);
+// console.log(newnum);
+// console.log(num2);
+//var num = 15;
+//var newnum = ++num;//prefix increment operator means the variable is incfemented first and then
+//var newnum = ++num + 5;
+// console.log(num);//the expression is evaluated using the new variable
+// console.log(newnum);
+//COMPARISION OPERATOR
+ //var a = 30;
+ //var b = 10;
+// console.log(a==b);
+// console.log(a!=b);//not eqaul
+// console.log(a>=b);//greater than eqaul to
+// console.log(a<=b);//less than eqaul to
+//LOGICAL OPERATOR
+//&& :-AND
+//example: a>b && b>0 && b<a   //evry expression should be true then the logical operators work
+// console.log(a>b && b>0 );
+// console.log(a>b && b>0 && b>0 );
+// // OR operator - ||   //only one or more expression is ture.
+// console.log(a>b || b>0 );
+// console.log(a>b || b<0 || b>0 ); 
+// //NOT operator - ! //makes truth to falsity and vice versa.
+//console.log(!(a>0 || b>0)) ;
+//string concatenation
+//console.log("hello" + "world");
+//console.log(5 + " world" );
+//console.log(3**4 );//power of 3 means 3*3*3*3 alled expontial operator
+// console.log(10 ** -1 ); //means 1/10
+// console.log(5 -"rosy" ); //concatenation does not work on minus sign so output will be NaN
+// a = 5;
+// b = 10;
+// var c = b;
+// console.log( "the value of c is "+c +"." );
+// var num1 = 5;
+// var num2 = '5';
+// console.log(type0f(num1) );
+// console.log(typeof(num2) );
+// console.log(num1 == num2 ); //output - true bcoz == sign only checks the value 
+// var num1 = 5;
+//  var num2 = '5';
+//  console.log(typeof(num1) );
+// console.log(typeof(num2) );
+//  console.log(num1 == num2 ); //output - false bcoz === sign checks the value with datatype
+//CONTROL STATEMENT AND LOOPS
+//IF-ELSE
+/*var tomorrow = 'rain';
+if (tomorrow == 'rain') {
+    console.log('Bring your raincoat.');  
+}
+else
+{
+    console.log('No need to bring your raincoat.');  
+}
+var tomorrow = 'sunny';
+if (tomorrow == 'rain') {
+    console.log('Bring your raincoat.');  
+}
+else
+{
+    console.log('No need to bring your raincoat.');  
+}*/
+var year = 2020;
+if (year%4 ==  0){
+{
+
+}
+else
+{
+    console.log("The year" + year + " is not a leap year.");}
+
+}
+else
+{
+    console.log("The year" + year + " is not a leap year.");
+}
+ 
+    
+
+
+
